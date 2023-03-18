ecdbcff149c3e6290510dd13c27f86aa3bd9e15d
Visuvasi0 <118104924+Visuvasi0@users.noreply.github.com>
2023-03-18 11:04:29 +0530

    Update DriverServiceImpl.java

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -12,27 +12,49 @@ import com.driver.repository.DriverRepository;
 @Service
 public class DriverServiceImpl implements DriverService {
 
-	@Autowired
-	DriverRepository driverRepository3;
+    @Autowired
+    DriverRepository driverRepository3;
 
-	@Autowired
-	CabRepository cabRepository3;
+    @Autowired
+    CabRepository cabRepository3;
 
-	@Override
-	public void register(String mobile, String password){
-		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+    @Autowired
+    AdminServiceImpl adminServiceImpl;
 
-	}
+    @Override
+    public void register(String mobile, String password){
+        //Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+        Driver driver = new Driver();
 
-	@Override
-	public void removeDriver(int driverId){
-		// Delete driver without using deleteById function
+        Cab cab = new Cab();
+        cab.setAvailable(true);
+        cab.setPerKmRate(10);
 
-	}
+        driver.setMobile(mobile);
+        driver.setPassword(password);
+        driver.setCab(cab);
 
-	@Override
-	public void updateStatus(int driverId){
-		//Set the status of respective car to unavailable
+        driverRepository3.save(driver);
+    }
 
-	}
+    @Override
+    public void removeDriver(int driverId){
+        // Delete driver without using deleteById function
+        Driver driver = driverRepository3.findById(driverId).get();
+        driverRepository3.delete(driver);
+
+    }
+
+    @Override
+    public void updateStatus(int driverId){
+        //Set the status of respective car to unavailable
+        Driver driver = driverRepository3.findById(driverId).get();
+
+        Cab cab = driver.getCab();
+        cab.setAvailable(false);
+        driver.setCab(cab);
+
+        driverRepository3.save(driver);
+
+    }
 }
